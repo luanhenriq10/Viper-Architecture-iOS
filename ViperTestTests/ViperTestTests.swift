fb8a9b39a1d581d20f701d499fa4bd3e587f09d0
Luan Henrique Damasceno Costa <LuanDamasceno@macbook-pro-de-luan.local>
2016-10-13 11:35:10 -0300

    Initial Commit

---
+++ ViperTestTests/ViperTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  ViperTestTests.swift
+//  ViperTestTests
+//
+//  Created by Luan Henrique Damasceno Costa on 13/10/16.
+//  Copyright © 2016 Luan Henrique Damasceno Costa. All rights reserved.
+//
+
+import XCTest
+@testable import ViperTest
+
+class ViperTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
